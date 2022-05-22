61ba3fae8a36a5e032c14ed9de3b2128ecda64b6
InesSehili <inessehili96@gmail.com>
2022-05-22 19:46:57 +0100

    add to cart remove from cart

--- core/models.py
+++ core/models.py
@@ -1,28 +1,83 @@
 from django.db import models
 from django.conf import settings
-# Create your models here.
+from django.shortcuts import get_object_or_404, redirect, reverse
+from django.utils import timezone
+
+CATEGORY_CHOICES = (
+    ('S', 'Shirt'),
+    ('SW', 'Sport wear'),
+    ('OW', 'Outwear')
+)
+
+LABEL_CHOICES = (
+    ('p', 'primary'),
+    ('s', 'secondary'),
+    ('d', 'danger')
+)
+
 
 class Item(models.Model):
-	title = models.CharField(max_length=100)
-	price = models.FloatField() 
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_price = models.FloatField(blank=True, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    label = models.CharField(choices=LABEL_CHOICES, max_length=1)
+    slug = models.SlugField()
+    description = models.TextField()
 
-	def __str__(self):
-		return self.title
-	
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("core:product", kwargs={"slug": self.slug})
+
+    def get_add_to_cart_url(self):
+        return reverse("core:add-to-cart", kwargs={"slug": self.slug})
+
 
 class OrderItem(models.Model):
-	item = models.ForeignKey(Item , 
-		on_delete = models.CASCADE)
-	def __str__(self):
-		return self.title
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    item = models.ForeignKey(Item,
+                             on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    ordred = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item}"
+
 
 class Order(models.Model):
-	user = models.ForeignKey(settings.AUTH_USER_MODEL, 
-		on_delete = models.CASCADE)
-	items = models.ManyToManyField(OrderItem)
-	start_date = models.DateTimeField(auto_now_add = True)
-	ordered_date = models.DateTimeField()
-	ordred = models.BooleanField(default = False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordred = models.BooleanField(default=False)
 
-	def __str__(self):
-		return self.user.username
+    def __str__(self):
+        return self.user.username
+
+
+def add_to_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item, user=request.user, ordred=False)
+    order_qs = Order.objects.filter(user=request.user, ordred=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+        else:
+            order.items.add(order_item)
+    else:
+        order = Order.objects.create(
+            user=request.user, ordered_date=timezone.now())
+        order.items.add(order_item)
+    return redirect("core:product", slug=slug)
+
+
+def remove_from_cart(request, slug):
+
+    return redirect("core:product", slug=slug)
--- core/views.py
+++ core/views.py
@@ -1,9 +1,31 @@
+
 from django.shortcuts import render
+from django.views.generic import ListView, DeleteView
 from .models import Item
-# Create your views here.
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = "home-page.html"
+
+
+class ItemDetailView(DeleteView):
+    model = Item
+    template_name = "product-page.html"
+
 
 def item_list(request):
-	context = {
-	'items': Item.objects.all()
-	}
-	return render(request, "home-page.html", context)
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "home-page.html", context)
+
+
+def products(request):
+    context = {}
+    return render(request, "product-page.html", context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, "checkout-page.html", context)
